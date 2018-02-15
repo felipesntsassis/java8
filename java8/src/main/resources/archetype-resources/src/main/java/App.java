c9e2a25ab3cd57ea13c4ab08716c1744a1e2f1f3
Felipe dos S. Assis <felipe.assis@sigma.com.br>
2018-02-15 17:02:41 -0200

    Setup inicial do projeto.

---
+++ java8/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $br.com.a3.java8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
